2dd02e007cacaf2d83ef315c05e37b4b1f076922
Luis-Felipe-N <luisj2felip09@gmail.com>
2022-07-30 10:17:23 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,35 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import { FormEvent, useState } from 'react'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  const [email, setEmail] = useState('')  
+  const [password, setPassword] = useState('')  
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault()
+
+    const data = {
+      email,
+      password
+    }
+
+    console.log(data)
+  }
+
+
+  return (
+    <div className={styles.container}>
+      <form onSubmit={handleSubmit}>
+        <input type="email" name="email" value={email} onChange={(event) => setEmail(event.target.value)} />
+        <input type="password" name="password" value={password} onChange={(event) => setPassword(event.target.value)} />
+
+        <button type="submit">Entrar</button>
+      </form>
+    </div>
+  )
+}
+
+export default Home
